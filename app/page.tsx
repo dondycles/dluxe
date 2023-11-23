f8d5ef7a0d4bacb47b1664aabafb5b2582380ad2
dondycles <johnrod.dondoyano@gmail.com>
2023-11-23 16:54:49 +0800

    modified:   app/page.tsx 	modified:   app/providers.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import slide1 from "@/public/slide1.webp";
 import slide2 from "@/public/slide2.webp";
-import { Button } from "@nextui-org/react";
+import { Button, Divider } from "@nextui-org/react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
@@ -35,45 +35,70 @@ export default function Home() {
     },
   ];
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prev) => (prev === contents.length - 1 ? 0 : prev + 1));
-    }, 5000);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setIndex((prev) => (prev === contents.length - 1 ? 0 : prev + 1));
+  //   }, 5000);
 
-    return () => clearInterval(interval);
-  }, []);
+  //   return () => clearInterval(interval);
+  // }, []);
   return (
-    <main className="w-full h-full overflow-x-hidden overflow-y-auto pt-32 px-4 sm:px-16 lg:px-32">
-      <div className="relative">
-        <AnimatePresence>
-          <m.div
-            initial={{ opacity: 0, translateX: 20 }}
-            animate={{ opacity: 1, translateX: 0 }}
-            exit={{ opacity: 0, translateX: 20 }}
-            key={index}
-            className="flex-1 shrink-0 flex flex-wrap gap-4  items-end justify-center absolute top-0 left-0 w-full "
-          >
+    <main className="min-h-full h-full flex flex-col ">
+      <AnimatePresence mode="popLayout">
+        <m.div
+          initial={{ opacity: 0, translateX: 20 }}
+          animate={{ opacity: 1, translateX: 0 }}
+          exit={{ opacity: 0, translateX: 20 }}
+          key={index}
+          className="flex flex-col-reverse lg:flex-row gap-x-4 items-center justify-end w-full h-fit "
+        >
+          <div className="min-h-[300px] max-h-[500px] h-screen flex-1 w-full relative ">
             <Image
-              className="max-w-[400px] w-full h-auto "
               src={contents[index].img}
-              alt="Slide2"
+              alt=""
+              fill
+              className="object-contain object-bottom h-full w-full"
             />
-            <div className="flex flex-col gap-2 max-w-[400px]">
-              <p className="text-4xl font-semibold text-primary">
-                {contents[index].title}
-              </p>
-              <p>{contents[index].description}</p>
-              <Button
-                className="text-xs font-semibold text-white"
-                color="primary"
-                variant="shadow"
-                radius="full"
-              >
-                Get Started
-              </Button>
-            </div>
-          </m.div>
-        </AnimatePresence>
+          </div>
+          <div className="flex flex-col gap-2 max-w-[400px] mt-auto ">
+            <p className=" text-3xl sm:text-5xl font-semibold text-primary">
+              {contents[index].title}
+            </p>
+            <p>{contents[index].description}</p>
+            <Button
+              className="text-xs font-semibold text-white"
+              color="primary"
+              variant="shadow"
+              radius="full"
+            >
+              Get Started
+            </Button>
+          </div>
+        </m.div>
+      </AnimatePresence>
+      <div className="flex flex-row gap-4 mt-8 w-full  ">
+        <div className="flex gap-4 flex-row flex-1 bg-background/50 p-4 rounded-xl shadow-lg">
+          <div className="flex-1">
+            <p className="font-semibold text-primary text-xl">$7T</p>
+            <p>
+              Projected global investment in direct digital transformation
+              efforts from 2020 to 2023.
+            </p>
+          </div>
+          <div className="flex-1">
+            <p className="font-semibold text-primary text-xl">$11T</p>
+            <p>
+              Projected transaction value of the digital payments market by
+              2026.
+            </p>
+          </div>
+          <div className="flex-1">
+            <p className="font-semibold text-primary text-xl">$384B</p>
+            <p>
+              Projected size of the global blockchain technology market by 2028.
+            </p>
+          </div>
+        </div>
       </div>
     </main>
   );
